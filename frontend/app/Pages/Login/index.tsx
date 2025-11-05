1c9bec1cef1a92a3731775ec513b0a01c70beeb9
dungnq271 <dungnguyen2712002@gmail.com>
2025-11-05 23:07:16 +0700

    Optimize the login and register page
    Use clientAction function of react-router in replacement of
    useMutation
    Add custom validity feature for password and confirm password field in
    register page. Specifically, it will check if password length is too
    short (<8 characters) and password and confirm password similarity
    Add required attribute to each fields in replacement of setting it in
    the register function from useForm
    Add pending UI in submit button for the pages
    Convert the field name to label tag for easier testing later
    Change to only disable the submit button when submitting in progress

---
+++ frontend/app/Pages/Login/index.tsx
@@ -0,0 +1,119 @@
+import type { Route } from './+types/index';
+import { useState, useEffect } from 'react';
+import { Form, Link, useNavigate, useSubmit, useActionData, useNavigation } from 'react-router';
+import { useForm, useWatch } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+import PasswordInput from '../../Components/PasswordInput/PasswordInput';
+import { login } from '../../utils/apis';
+import { useAuth } from '../../Providers/authProvider';
+import './index.css';
+import Separation from '../../Components/Separation/Separation';
+
+const LoginUserSchema = z.object({
+  email: z.email({ message: 'Please enter a valid email address' }),
+  password: z.string().min(1, { message: 'Please enter a password' })
+});
+
+export type LoginUserSchemaType = z.infer<typeof LoginUserSchema>;
+
+export async function clientAction({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    const response = await login(data);
+    return {
+      success: true,
+      response
+    };
+  } catch (err: any) {
+    return {
+      success: false,
+      response: err.response
+    };
+  }
+}
+
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<LoginUserSchemaType>({
+    resolver: zodResolver(LoginUserSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  });
+
+  const { setUser, setJwt, setRefreshToken } = useAuth();
+  const submit = useSubmit();
+  const navigate = useNavigate();
+  const navigation = useNavigation();
+  const data = useActionData();
+  const errorMsg = data && !data.success && data?.response.data.message;
+
+  useEffect(() => {
+    if (data && data.success && data.response) {
+      const response = data.response;
+      setUser(response.data.user);
+      setJwt(response.data.jwt);
+      setRefreshToken(response.data.refreshToken);
+      navigate('/');
+    }
+  }, [data]);
+
+  const onSubmit = (data) => {
+    submit(data, { method: 'post' });
+  };
+
+  const isSubmitting = navigation.state === 'submitting';
+  // TODO: add pending UI for this state
+  const isLoading = navigation.state === 'loading';
+  const isDisabled = isSubmitting; // Prevents double-submit
+
+  return (
+    <div id="login-page">
+      <div id="modal">
+        <h1>Sign in</h1>
+        <Form id="login-form" method="post" onSubmit={handleSubmit(onSubmit)}>
+          {!errors.email && !errors.password && errorMsg && <p className="err">{errorMsg}</p>}
+          <div id="email">
+            <label htmlFor="email-input">Email</label>
+            <input id="email-input" required {...register('email')} />
+            {errors.email && <span className="err">{errors.email.message}</span>}
+          </div>
+          <div id="password">
+            <div>
+              <label htmlFor="password-input">Password</label>
+              <Link to="/signup">Forgot password?</Link>
+            </div>
+            <PasswordInput required {...register('password')} />
+            {errors.password && <span className="err">{errors.password.message}</span>}
+          </div>
+          <button
+            type="submit"
+            disabled={isDisabled}
+            className={'submit-btn' + (isSubmitting ? ' progress' : '')}
+          >
+            {isSubmitting ? 'Sign in...' : 'Sign in'}
+          </button>
+          <Separation />
+          <button id="with-google" type="submit" className="submit-btn">
+            Continue with Google
+          </button>
+        </Form>
+        <div id="create-account">
+          <p>
+            New?{' '}
+            <Link id="to_register" to="/register">
+              Create an account
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/app/Pages/Register/index.tsx
@@ -0,0 +1,224 @@
+import type { Route } from './+types/index';
+import { useEffect } from 'react';
+import { Form, Link, useSubmit, useActionData, useNavigation, redirect } from 'react-router';
+import { useForm, useWatch } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+import { register as requestRegisterApi } from '../../utils/apis';
+import './index.css';
+import PasswordInput from '../../Components/PasswordInput/PasswordInput';
+import Separation from '../../Components/Separation/Separation';
+
+const RegisterUserSchema = z
+  .object({
+    firstName: z.string().min(1, { message: 'Please enter first name' }),
+    lastName: z.string().min(1, { message: 'Please enter first name' }),
+    username: z.string().min(1, { message: 'Please enter username' }),
+    email: z.email({ message: 'Please enter a valid email address' }),
+    password: z
+      .string()
+      .min(1, { message: 'Please enter a password' })
+      .min(8, { message: 'Password must be at least 8 characters' }),
+    confirmPassword: z
+      .string()
+      .min(1, { message: 'Please confirm your password' })
+      .min(8, { message: 'Password must be at least 8 characters' })
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword']
+  });
+
+type RegisterUserSchemaType = z.infer<typeof RegisterUserSchema>;
+
+// Password validation patterns
+const passwordValidationPatterns = {
+  atLeastOneUppercase: /[A-Z]/,
+  atLeastOneLowercase: /[a-z]/,
+  atLeastOneNumeric: /[0-9]/,
+  atLeastOneSpecialChar: /[!@#\$%\^\&*\)\(+=._-]/
+};
+
+export async function clientAction({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    const response = await requestRegisterApi(data);
+    console.log(response);
+    return redirect('/login');
+  } catch (err: any) {
+    return {
+      success: false,
+      response: err.response
+    };
+  }
+}
+
+export default function Register() {
+  const {
+    control,
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors }
+  } = useForm<RegisterUserSchemaType>({
+    resolver: zodResolver(RegisterUserSchema),
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  });
+  const submit = useSubmit();
+  const navigation = useNavigation();
+  const data = useActionData();
+
+  useEffect(() => {
+    if (data && !data.success && data.response) {
+      Object.entries(data.response.data.errors).forEach((err: any) => {
+        setError(err[0], { type: 'manual', message: err[1][0] });
+      });
+    }
+  }, [data]);
+
+  const onSubmit = async (data) => {
+    submit(data, { method: 'post' });
+  };
+
+  const password: string = useWatch({
+    control,
+    name: 'password'
+  });
+
+  const countPasswordCritMatch = Object.entries(passwordValidationPatterns).filter(([_, value]) =>
+    value.test(password)
+  ).length;
+
+  const passwordStrength =
+    countPasswordCritMatch <= 1 ? 'low' : countPasswordCritMatch === 2 ? 'medium' : 'strong';
+
+  const isSubmitting = navigation.state !== 'idle';
+  const isDisabled = isSubmitting; // Prevents double-submit
+
+  return (
+    <div id="register-page">
+      <div id="left"></div>
+      <div id="right">
+        <div id="modal">
+          <h1>Create an account</h1>
+          <Form id="register-form" method="post" onSubmit={handleSubmit(onSubmit)}>
+            <div className="name">
+              <div id="first-name">
+                <label htmlFor="first-name-input">
+                  First name <span className="required-asterisk">*</span>
+                </label>
+                <input id="first-name-input" required {...register('firstName')} />
+                {errors.firstName && <span className="err">{errors.firstName.message}</span>}
+              </div>
+              <div id="last-name">
+                <label htmlFor="last-name-input">
+                  Last name <span className="required-asterisk">*</span>
+                </label>
+                <input id="last-name-input" required {...register('lastName')} />
+                {errors.lastName && <span className="err">{errors.lastName.message}</span>}
+              </div>
+            </div>
+            <div id="username">
+              <label htmlFor="username-input">
+                Username <span className="required-asterisk">*</span>
+              </label>
+              <input id="username-input" required {...register('username')} />
+              {errors.username && <span className="err">{errors.username.message}</span>}
+            </div>
+            <div id="email">
+              <label htmlFor="email-input">
+                Email <span className="required-asterisk">*</span>
+              </label>
+              <input id="email-input" required {...register('email')} />
+              {errors.email && <span className="err">{errors.email.message}</span>}
+            </div>
+            <div id="password">
+              <label htmlFor="password-input">
+                Password <span className="required-asterisk">*</span>
+              </label>
+              <PasswordInput
+                {...register('password', {
+                  required: true,
+                  onBlur: (e: React.ChangeEvent<HTMLInputElement>) => {
+                    e.target.reportValidity();
+                  },
+                  onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+                    // if password length < 8 set and error
+                    const value = e.target.value;
+                    if (value.length < 8) {
+                      e.target.setCustomValidity('Password must be at least 8 characters');
+                    } else {
+                      e.target.setCustomValidity('');
+                    }
+                  }
+                })}
+              />
+              {errors.password ? (
+                <span className="err">{errors.password.message}</span>
+              ) : (
+                password.length > 0 && (
+                  <span className="password-strength" id={passwordStrength}>
+                    Strength: {passwordStrength}
+                  </span>
+                )
+              )}
+            </div>
+            <div id="confirm-password">
+              <label htmlFor="confirm-password-input">
+                Confirm password <span className="required-asterisk">*</span>
+              </label>
+              <input
+                id="confirm-password-input"
+                type="password"
+                required
+                {...register('confirmPassword', {
+                  onBlur: (e: React.ChangeEvent<HTMLInputElement>) => {
+                    e.target.reportValidity();
+                  },
+                  onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+                    // if password length < 8 set and error
+                    const value = e.target.value;
+                    if (value != password) {
+                      e.target.setCustomValidity('Password does not match');
+                    } else {
+                      e.target.setCustomValidity('');
+                    }
+                  }
+                })}
+              />
+              {errors.confirmPassword && (
+                <span className="err">{errors.confirmPassword.message}</span>
+              )}
+            </div>
+            <button
+              id="register"
+              type="submit"
+              disabled={isDisabled}
+              className={'submit-btn' + (isSubmitting ? ' progress' : '')}
+            >
+              {isSubmitting ? 'Create account...' : 'Create account'}
+            </button>
+            <Separation />
+            <div id="already-have-account">
+              <span>
+                Already have an account?{' '}
+                <Link id="to_login" to="/login">
+                  &rarr; Login
+                </Link>
+              </span>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
